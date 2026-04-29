6fba5835d62682b4f958f56f01a14bc4d4fd3749
DaniHogg <daniel.hogg1992@gmail.com>
2026-04-29 21:13:42 +1000

    fixed syntax error, had JS regex literals in python

--- test-py/basic.py
+++ test-py/basic.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 from playwright.sync_api import expect
 
@@ -5,12 +7,12 @@ from playwright.sync_api import expect
 class TestGuildWars2Wiki:
     def test_homepage_loads_correctly(self, page):
         page.goto('https://wiki.guildwars2.com/')
-        expect(page).to_have_title(/Guild Wars/)
+        expect(page).to_have_title(re.compile(r'Guild Wars'))
         expect(page.get_by_text('Guild Wars 2')).to_be_visible()
 
     def test_wiki_page_loads_correctly(self, page):
         page.goto('https://wiki.guildwars2.com/wiki/Main_Page')
-        expect(page).to_have_title(/Guild Wars/)
+        expect(page).to_have_title(re.compile(r'Guild Wars'))
         expect(page.get_by_text('Main')).to_be_visible()
 
     def test_nonexistent_page_handling(self, page):
